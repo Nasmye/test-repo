5bed2cd68639946c78401abda0c94b531082a4fe
Nasmye <yasmine.chenafa@gmail.com>
2021-03-10 11:04:51 +0100

    test 2

--- calcul.py
+++ calcul.py
@@ -1,4 +1,4 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Sed non risus. Suspendisse lectus tortor, dignissim sit amet,
+Sed non risus.  ddcdc   Suspendisse lectus tortor, dignissim sit amet,
  adipiscingec, ultricies sed, dolor. Cras elementum ultrices 
- diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi
+ diam. Maecenas ligula massa, varius a,  dcdcdc   semper congue, euismod non, mi
