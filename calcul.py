87eec9b709390ceceb1d78fe5c28f13cb80b7fcc
Nasmye <yasmine.chenafa@gmail.com>
2021-03-10 11:10:10 +0100

    Change

--- calcul.py
+++ calcul.py
@@ -1,4 +1,3 @@
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
 Sed non risus.  ddcdc   Suspendisse lectus tortor, dignissim sit amet,
- adipiscingec, ultricies sed, dolor. Cras elementum ultrices 
- diam. Maecenas ligula massa, varius a,  dcdcdc   semper congue, euismod non, mi
+-----
